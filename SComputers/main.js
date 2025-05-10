6ad5e0cf01042a838b689863b4880ef58368daa8
igorkll <frerther@gmail.com>
2025-05-11 01:47:48 +0300

    backup

--- SComputers/main.js
+++ SComputers/main.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js', { scope: '/SComputers/' })
+            .then((registration) => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch((error) => {
+                console.log('ServiceWorker registration failed: ', error);
+            });
+    });
+}
+
 const themeToggleButton = document.getElementById('theme-toggle');
 const themeStylesheet = document.querySelector('link[rel="stylesheet"]');
 
