df123453e90bd03641f8d50fb2743731e4e5bc69
igorkll <frerther@gmail.com>
2026-05-15 20:30:54 +0300

    Update content.js

--- logichub/content.js
+++ logichub/content.js
@@ -642,6 +642,19 @@ The project was built using gnubox maker: https://github.com/igorkll/Gnubox-Make
     ['Download', 'https://drive.google.com/file/d/1S_TPnbrVpCp6TOyJG5vLdvbxz-iLMLS_/view?usp=sharing']
 ], ['js'], null, states.SUPPORTED, 'Software');
 
+addCard('grammowavcli', 
+`converts a music file into a 3D model of a record for printing on a 3D printer and listening on a gramophone
+please note that model generation can take up to several hours
+printing is only possible on a photopolymer 3D printer, as FDM does not have enough quality
+it is not recommended to play printed records on an electric turntable, as this may damage the pickup
+the default program parameters have been calculated for printing 78 rpm records and further listening on the gramophone`,
+null, [
+    'https://raw.githubusercontent.com/igorkll/grammowavcli/refs/heads/main/preview.png'
+], 
+[
+    ['Project page', 'https://github.com/igorkll/grammowavcli']
+], ['python'], null, states.BETA, 'Software');
+
 // --------------------------------------------------------------- Services
 
 addCard('Scrap Mechanic Server', 
@@ -932,7 +945,16 @@ null, null,
 [
     ['Project page', 'https://github.com/igorkll/liamounts'],
     ['Download', 'liamounts', 'dlgithub_source_zip']
-], ['bash'], null, states.COMPLETED, 'Other');
+], ['bash', 'c'], null, states.COMPLETED, 'Other');
+
+addCard('linux-embedded-setup-scripts', 
+`scripts for configuring embedded linux from a running system can be executed from a target device or by automated build systems from a chroot/vm.
+all scripts must be run from root.`,
+null, null,
+[
+    ['Project page', 'https://github.com/igorkll/linux-embedded-setup-scripts'],
+    ['Download', 'linux-embedded-setup-scripts', 'dlgithub_source_zip']
+], ['bash'], null, states.WIP, 'Other');
 
 // --------------------------------------------------------------- Web
 
